869848e2b657c13b9e503e848e8a0ef6ce269f3b
sofidolores <sofiaanaeldolores@gmail.com>
2024-01-06 20:03:45 -0300

    cambios en el codigo

---
+++ Ejercicios/ejercicio3.py
@@ -0,0 +1,12 @@
+
+def switch_case(variableNotasMusicales):
+    match variableNotasMusicales:
+        case 1: print ("DO")
+        case 2: print ("RE")
+        case 3: print ("MI")
+        case 4: print ("FA")
+        case 5: print ("SOL")
+        case 6: print ("LA")
+        case 7: print ("SI")
+default: print ("no existe la nota")
+    
